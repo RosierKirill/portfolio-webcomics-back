da2dbabc668a8a7343e806888a66c3a6f0fa7d2b
kirill <kirill.rosier@lyceeastier.com>
2025-04-07 23:45:31 +0200

    version

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const userRoutes = require('./routes/users');
 app.use(cors()); 
 app.use(express.json());
 
-app.use('/api/characters', characterRoutes);
-app.use('/api/users', userRoutes);
+app.use('/characters', characterRoutes);
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
